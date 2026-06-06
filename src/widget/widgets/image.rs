9bb68caf9ccda4dc691fd24d40528fa8e4cd4102
Jake Stewart <jake.stephen.stewart@gmail.com>
2026-06-06 13:18:26 +0800

    rename image fill setter

--- src/widget/widgets/image.rs
+++ src/widget/widgets/image.rs
@@ -143,6 +143,6 @@ impl Image {
 
     crate::style_field! {
         /// Whether the image stretches to fill the area instead of letterboxing.
-        fill as fills: bool
+        fill: bool
     }
 }
